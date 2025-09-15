816ef5bd02e7fc3c56e58ec5ddcd46130cda68d0
Yegor <EPMironov@stud.kpfu.ru>
2025-09-15 20:30:00 +0300

    add: image transfer handler class

---
+++ V course/Digital image processing/laboratory work/core/first/image_transfer.py
@@ -0,0 +1,42 @@
+"""
+Классы для открытия и сохранения изображений
+"""
+from abc import ABC, abstractmethod
+from PIL import Image
+import numpy as np
+
+
+class IImageTransfer(ABC):
+    """
+    Абстрактный класс для представления класса взаимодействия с медиа файлами
+    """
+    @abstractmethod
+    def load_image(self, file_path: str) -> np.ndarray | None:
+        """Загрузить обрабатываемое изображение"""
+        pass
+
+    @abstractmethod
+    def save_image(self, array: np.ndarray, file_path: str) -> bool:
+        """Сохранить обрабатываемое изображение"""
+        pass
+
+
+class LocalImageTransfer(IImageTransfer):
+    """Класс для взаимодействия с медиа файлами"""
+
+    def load_image(self, file_path: str) -> np.ndarray | None:
+        """Загружает изображение в NumPy-массив (RGB)."""
+        try:
+            img = Image.open(file_path).convert("RGB")  # всегда 3 канала
+            return np.array(img)
+        except Exception as e:
+            print(f"Ошибка загрузки: {e}")
+            return None
+
+    def save_image(self, arr: np.ndarray, file_path: str) -> None:
+        """Сохраняет NumPy-массив (RGB) как изображение."""
+        try:
+            img = Image.fromarray(arr.astype(np.uint8), mode="RGB")
+            img.save(file_path)
+        except Exception as e:
+            print(f"Ошибка сохранения: {e}")
