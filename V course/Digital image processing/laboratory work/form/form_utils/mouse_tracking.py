282d57b416f3a6d75053dd83c7d80e735d417c6a
Yegor <EPMironov@stud.kpfu.ru>
2025-10-05 18:24:34 +0300

    fix: recalculation of coordinates based on image scaling

--- V course/Digital image processing/laboratory work/form/form_utils/mouse_tracking.py
+++ V course/Digital image processing/laboratory work/form/form_utils/mouse_tracking.py
@@ -1,3 +1,6 @@
+"""
+Вспомогательная форма для отслеживания движения курсора мыши
+"""
 from PyQt6.QtCore import Qt, pyqtSignal
 from PyQt6.QtWidgets import QLabel
 
@@ -12,19 +15,32 @@ class MouseTracker(QLabel):
 
     def mouseMoveEvent(self, event):
         """Обработчик движения курсора мыши"""
+
         pos = event.position()
-        self.mouse_moved.emit(pos.x(), pos.y())
+        # получаем реальный pixmap
+        pixmap = self.pixmap()
+        if pixmap:
+            scaled_w = pixmap.width()
+            scaled_h = pixmap.height()
+            label_w = self.width()
+            label_h = self.height()
+
+            offset_x = (label_w - scaled_w) // 2
+            offset_y = (label_h - scaled_h) // 2
+
+            # проверяем, попадает ли курсор в реальную область изображения
+            if offset_x <= pos.x() <= offset_x + scaled_w and offset_y <= pos.y() <= offset_y + scaled_h:
+                self.setCursor(Qt.CursorShape.CrossCursor)
+                # преобразуем координаты в координаты изображения
+                x_img = int((pos.x() - offset_x) * pixmap.width() / scaled_w)
+                y_img = int((pos.y() - offset_y) * pixmap.height() / scaled_h)
+                self.mouse_moved.emit(x_img, y_img)
+            else:
+                self.setCursor(Qt.CursorShape.ArrowCursor)
         super().mouseMoveEvent(event)
 
-    def enterEvent(self, event):
-        """Когда мышь заходит в область изображения — ставим крестик."""
-        self.setCursor(Qt.CursorShape.CrossCursor)
-        super().enterEvent(event)
 
     def leaveEvent(self, event):
         """Когда мышь выходит — возвращаем обычный курсор."""
         self.setCursor(Qt.CursorShape.ArrowCursor)
         super().leaveEvent(event)
-
-
-
